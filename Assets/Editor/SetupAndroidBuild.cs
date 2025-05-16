4c8d41a18c0777f7ee8de0fa33379bfb2b5b9f5c
lsa26 <151360195+lsa26@users.noreply.github.com>
2025-05-16 09:30:21 +0200

    Update SetupAndroidBuild.cs

--- Assets/Editor/SetupAndroidBuild.cs
+++ Assets/Editor/SetupAndroidBuild.cs
@@ -1,33 +1,44 @@
 using UnityEditor;
+using UnityEngine;
 using System.IO;
 using System.Collections.Generic;
+
 public class SetupAndroidBuild
 {
-public static void Configure()
-{
-// Changement de plateforme vers Android
-EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
-// Configuration des paramètres Android
-PlayerSettings.Android.bundleVersionCode = 1;
-PlayerSettings.bundleVersion = "1.0";
-PlayerSettings.companyName = "Demo";
-PlayerSettings.productName = "CloudBeesDemo";
-// Activer le autographics API pour Android
-PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.Android, false);
-List<GraphicsDeviceType> devices = new List<GraphicsDeviceType>();
-devices.Add(GraphicsDeviceType.OpenGLES3);
-PlayerSettings.SetGraphicsAPIs(BuildTarget.Android, devices.ToArray());
-// Configuration des scènes
-List<EditorBuildSettingsScene> scenes = new List<EditorBuildSettingsScene>();
-foreach (var sceneGUID in AssetDatabase.FindAssets("t:scene"))
-{
-string scenePath = AssetDatabase.GUIDToAssetPath(sceneGUID);
-scenes.Add(new EditorBuildSettingsScene(scenePath, true));
-}
-EditorBuildSettings.scenes = scenes.ToArray();
-// Sauvegarde des paramètres
-AssetDatabase.SaveAssets();
-// Log pour confirmer la configuration
-Debug.Log("Android build configuration completed");
-}
+    public static void Configure()
+    {
+        // Changement de plateforme vers Android
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+        
+        // Configuration des paramètres Android
+        PlayerSettings.Android.bundleVersionCode = 1;
+        PlayerSettings.bundleVersion = "1.0";
+        PlayerSettings.companyName = "Demo";
+        PlayerSettings.productName = "CloudBeesDemo";
+        
+        // Important : Désactiver le app bundle et configurer pour Gradle
+        EditorUserBuildSettings.buildAppBundle = false;
+        EditorUserBuildSettings.androidBuildSystem = AndroidBuildSystem.Gradle;
+        
+        // Activer le autographics API pour Android
+        PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.Android, false);
+        List<GraphicsDeviceType> devices = new List<GraphicsDeviceType>();
+        devices.Add(GraphicsDeviceType.OpenGLES3);
+        PlayerSettings.SetGraphicsAPIs(BuildTarget.Android, devices.ToArray());
+        
+        // Configuration des scènes
+        List<EditorBuildSettingsScene> scenes = new List<EditorBuildSettingsScene>();
+        foreach (var sceneGUID in AssetDatabase.FindAssets("t:scene"))
+        {
+            string scenePath = AssetDatabase.GUIDToAssetPath(sceneGUID);
+            scenes.Add(new EditorBuildSettingsScene(scenePath, true));
+        }
+        EditorBuildSettings.scenes = scenes.ToArray();
+        
+        // Sauvegarde des paramètres
+        AssetDatabase.SaveAssets();
+        
+        // Log pour confirmer la configuration
+        Debug.Log("Android build configuration completed");
+    }
 }
